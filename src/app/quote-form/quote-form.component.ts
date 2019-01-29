075d7b47a52e9ec29fa6a8efa5b01d5b450ccbcf
umutoni jacqueline <jacquelineumutoni13@gmail.com>
2019-01-29 11:31:12 +0200

    add a toogle button

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit,Output,EventEmitter} from '@angular/core';
 import {Quotes} from '../quotes';
 @Component({
   selector: 'app-quote-form',
@@ -7,8 +7,12 @@ import {Quotes} from '../quotes';
 })
 
 export class QuoteFormComponent implements OnInit {
-  @Input() quote:Quotes;
+  
   newQuote=new Quotes(0,"","",new Date());
+  @Output() addQuote=new EventEmitter<Quotes>();
+  submitGoal(){
+    this.addQuote.emit(this.newQuote);
+}
   constructor() { }
 
   ngOnInit() {
